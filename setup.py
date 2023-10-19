9f4672762799e9b7776439b4da747c76f6b0e80f
Eric Gjertsen <ericgj72@gmail.com>
2023-10-18 23:23:13 -0400

    version bump (0.3)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = ["pytest", "pytest-random-order"]
 
 setup(
     name="xfind",
-    version="0.2",
+    version="0.3",
     description="concurrent find files + exec",
     license="MIT",
     author="Eric Gjertsen",
